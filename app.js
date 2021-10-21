b8153734e88b5492b1d1912cf867e69ab8e047ca
Mariamopataraia <mariamopataraia@gmail.com>
2021-10-21 16:37:54 +0400

    Triangle

---
+++ app.js
@@ -0,0 +1,26 @@
+        // javavariables
+        Var
+        let
+        const
+        // examples
+        var myName = "Mariam"
+
+        // letArithmetics
+        let x = 100 + 50;
+        let x = (100 + 50) * a;
+    // String
+
+    const Example1 = "Sting Example";
+    const Example2 = 'Sting Example';
+    const Example3 = `Yet another string Sting Example`;
+    console.log(Example1); (Example2); (Example3)
+
+    // Triangle Area
+    var side1 = 5; 
+    var side2 = 6; 
+    var side3 = 6; 
+    var s = (side1 + side2 + side3)/2;
+    var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+    console.log(area);
+
+    
